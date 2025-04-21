eb913c93066360c38a19e18e1b96f19c3761c9d2
theOMEN203 <69845492+theOMEN203@users.noreply.github.com>
2025-04-21 13:53:33 +0530

    Create landing.js

---
+++ views/landing.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            text-align: center;
+            margin-top: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to our site!</h1>
+    <p>Your IP: <%= ip %></p>
+    <p>Country: <%= country %></p>
+</body>
+</html>
