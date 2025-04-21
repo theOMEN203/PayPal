535df8d26a1044c8953ec5cf63df1ae2d9007875
theOMEN203 <69845492+theOMEN203@users.noreply.github.com>
2025-04-21 15:05:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,34 +5,42 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Replace this with your token from ipinfo.io
-const IPINFO_TOKEN = 'bfc20db1153926';
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', async (req, res) => {
     const userIP = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
+    console.log(`Incoming Request IP: ${userIP}`);
 
     try {
-        const response = await axios.get(`https://ipinfo.io/${userIP}?token=${IPINFO_TOKEN}`);
+        const response = await axios.get(`http://ip-api.com/json/${userIP}?fields=status,message,country,regionName,city,isp,org,proxy,hosting,query`);
         const data = response.data;
 
-        const isVPN = data.privacy?.vpn || false;
-
-        if (isVPN) {
-            const log = `[VPN DETECTED] IP: ${data.ip} | ISP: ${data.org} | Country: ${data.country} | Location: ${data.city}\n`;
-            console.log(log);
-            fs.appendFileSync('vpn-log.txt', log);
+        if (data.status !== 'success') {
+            console.log(`IP lookup failed: ${data.message}`);
+            return res.send('Could not fetch your IP data.');
         }
 
+        const isVPN = data.proxy || data.hosting;
+
+        // Log every visit with VPN status
+        const log = `[VISIT] IP: ${data.query} | Country: ${data.country} | City: ${data.city || 'N/A'} | ISP: ${data.org || 'N/A'} | VPN: ${isVPN}\n`;
+        console.log(log);
+        fs.appendFileSync('vpn-log.txt', log);
+
+        if (isVPN) {
+            console.log(`[ALERT] VPN detected for IP ${data.query}`);
+        }
+
+        // Render the landing page with info
         res.render('landing', {
-            ip: data.ip,
+            ip: data.query,
             country: data.country,
+            vpn: isVPN ? 'Yes' : 'No'
         });
 
     } catch (err) {
-        console.error("Error fetching IP info:", err.message);
+        console.error('Error fetching IP info:', err.message);
         res.send('Something went wrong.');
     }
 });
