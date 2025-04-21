2806f35f61c008163bc3a71eaa9084d0351a018a
theOMEN203 <69845492+theOMEN203@users.noreply.github.com>
2025-04-21 15:01:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Replace this with your token from ipinfo.io
+const IPINFO_TOKEN = 'bfc20db1153926';
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -12,20 +15,19 @@ app.get('/', async (req, res) => {
     const userIP = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
 
     try {
-        const response = await axios.get(`https://ip-api.com/json/${userIP}?fields=status,message,country,regionName,city,isp,org,proxy,hosting,query`);
+        const response = await axios.get(`https://ipinfo.io/${userIP}?token=${IPINFO_TOKEN}`);
         const data = response.data;
 
-        const isVPN = data.proxy || data.hosting;
+        const isVPN = data.privacy?.vpn || false;
 
         if (isVPN) {
-            const timestamp = new Date().toISOString();
-            const log = `[${timestamp}] [VPN DETECTED] IP: ${data.query} | ISP: ${data.isp} | Country: ${data.country} | Host: ${data.hosting}\n`;
+            const log = `[VPN DETECTED] IP: ${data.ip} | ISP: ${data.org} | Country: ${data.country} | Location: ${data.city}\n`;
             console.log(log);
             fs.appendFileSync('vpn-log.txt', log);
         }
 
         res.render('landing', {
-            ip: data.query,
+            ip: data.ip,
             country: data.country,
         });
 
